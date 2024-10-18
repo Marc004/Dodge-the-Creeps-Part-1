d3b7ed4327b51c7da842d8bdd14bef9d83be4fb9
Marc004 <marcvidal234@gmail.com>
2024-10-18 16:40:40 +0200

    Finish

--- Ex 6/Player.gd
+++ Ex 6/Player.gd
@@ -1,17 +1,21 @@
 extends Area2D
 
-export var speed = 400 # A quina velocitat es mourà el jugador (píxels/seg). var screen_size # Mida de la finestra de joc
+export var speed = 400 # A quina velocitat es mourà el jugador (píxels/seg).
+var screen_size # Mida de la finestra de joc
 
 func _ready():
-	print("hola")
 	screen_size = get_viewport_rect().size
 
 func _process(delta):
-	var velocity = Vector2.ZERO # Vector de moviment del jugador. if Input.is_action_pressed("move_right"):
-	velocity.x += 1
-	if Input.is_action_pressed("move_left"): velocity.x -= 1
-	if Input.is_action_pressed("move_down"): velocity.y += 1
-	if Input.is_action_pressed("move_up"): velocity.y -= 1
+	var velocity = Vector2.ZERO # Vector de moviment del jugador.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
